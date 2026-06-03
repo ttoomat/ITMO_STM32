5e5091e52165b09e8f1d21e60bf78e7fca06580d
Troitskaya Tamara <troitskayatamara@mail.ru>
2026-06-03 13:04:12 +0300

    Sawtooth gen with ext DAC (8830). Problem: high noise (see the graph).

--- F411_DAC/Core/Src/main.c
+++ F411_DAC/Core/Src/main.c
@@ -1,6 +1,6 @@
 /* File: main.c
  * Date: 15.04.2026
- * Purpose: external DAC sine wave generation project.
+ * Purpose: external DAC sawtooth wave generation.
  * External DAC: DAC8830
  * Vref (for Vpow=3.1V): 1.24 V
  */
@@ -9,37 +9,26 @@
 #define GPIO_AFRL_AFRL4_AF5 (5U << 16U)
 #define GPIO_AFRL_AFRL5_AF5 (5U << 20U)
 #define GPIO_AFRL_AFRL7_AF5 (5U << 28U)
-#define APB2_FREQ (16000000UL)
+#define APB2_FREQ (16000000UL) // APB2 default frequency
+volatile uint16_t data = 0x0000; // SPI data for external DAC
+volatile uint8_t data_changed = 0;
 
-// PC13 led on
-void LED_Setup() {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-	// MODE13 = 01, General purpose output
-	GPIOC->MODER &= ~(GPIO_MODER_MODE13);
-	GPIOC->MODER |= (GPIO_MODER_MODE13_0);
-}
-
-// inverted LED logic
-void LED_off() {
-	GPIOC->ODR |= (GPIO_ODR_ODR_13);
-}
-
-void LED_on() {
-	GPIOC->ODR &= ~(GPIO_ODR_ODR_13);
-}
-
-void LED_toggle() {
-	GPIOC->ODR ^= (GPIO_ODR_ODR_13);
-}
+#define CS_LOW()  (GPIOA->BSRR = GPIO_BSRR_BR_4)
+#define CS_HIGH() (GPIOA->BSRR = GPIO_BSRR_BS_4)
 
 void SPI1_Send(uint16_t data) {
 	// SPI enable -- NSS goes from 1 to 0
+	CS_LOW();
 	//SPI1->CR1 |= SPI_CR1_SPE;
 	while (!(SPI1->SR & SPI_SR_TXE));
 	SPI1->DR = data;
-	// waiting until the end of transmition
+	while (!(SPI1->SR & SPI_SR_TXE));  // ← добавить: ждём, пока уйдёт в shift register
 	while (SPI1->SR & SPI_SR_BSY);
+	volatile uint16_t dummy = SPI1->DR; // ← сбросить RXNE/OVR
+	// waiting until the end of transmition
+	//while (SPI1->SR & SPI_SR_BSY);
 	//SPI1->CR1 &= ~SPI_CR1_SPE;
+	CS_HIGH();
 }
 
 // SPI1 is on APB2 bus
@@ -65,21 +54,50 @@ void SPI1_Setup() {
 	SPI1->CR1 |= SPI_CR1_DFF;
 	// master selection
 	SPI1->CR1 |= SPI_CR1_MSTR;
+	SPI1->CR1 |= SPI_CR1_CPHA;  // falling edge
 	// NSS software (программный)
-	//SPI1->CR1 |= SPI_CR1_SSM;
-	//SPI1->CR1 |= SPI_CR1_SSI;
+	SPI1->CR1 |= SPI_CR1_SSM;
+	SPI1->CR1 |= SPI_CR1_SSI;
 	// NSS hardware (аппаратный)
-	SPI1->CR2 |= SPI_CR2_SSOE;
+	//SPI1->CR2 |= SPI_CR2_SSOE;
+	// CS
+	GPIOA->MODER &= ~GPIO_MODER_MODE4;
+	GPIOA->MODER |= GPIO_MODER_MODE4_0;
 	// SPI enable
 	SPI1->CR1 |= SPI_CR1_SPE;
 }
 
+void SysTick_Init() {
+	// до скольки он считает
+	SysTick->LOAD = (uint32_t)(100-1);
+	// internal clock
+	SysTick->CTRL |= (1U << 2);
+	// exception when counts down zero
+	SysTick->CTRL |= (1U << 1);
+	// Enable
+	SysTick->CTRL |= (1U << 0);
+	NVIC_EnableIRQ(SysTick_IRQn);
+}
+
+// when SysTick counts down zero, increase DAC data
+void SysTick_Handler() {
+	if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) {
+		if (data < 0xFFFF) {
+		    data += 0x1;
+		} else {
+			data = 0x0;
+		}
+		data_changed = 1;
+	}
+}
+
 int main() {
-	//LED_Setup();
 	SPI1_Setup();
-	uint16_t data = 0xf000;//0xFFFF;
-	//SPI1_Send(data);
+	SysTick_Init();
 	while(1) {
-		SPI1_Send(data);
+		if (data_changed) {
+			SPI1_Send(data);
+			data_changed = 0;
+		}
 	}
 }
