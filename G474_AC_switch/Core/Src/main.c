09d8071417a834a55c59e0b73c64a29acdb05be3
Troitskaya Tamara <troitskayatamara@mail.ru>
2026-06-26 19:43:28 +0300

    External DAC sin. Problem: too much noise.

--- G474_AC_switch/Core/Src/main.c
+++ G474_AC_switch/Core/Src/main.c
@@ -14,21 +14,70 @@
 #define PWM_desired_freq (1000U)
 
 // SPI1
-#define DAC_Saw_freq (50U)
-#define MAX_DAC_DATA (655U)
+//#define DAC_Saw_freq (50U)
+//#define MAX_DAC_DATA (655U)
 #define GPIO_AFRL_AFRL4_AF5 (5U << 16U)
 #define GPIO_AFRL_AFRL5_AF5 (5U << 20U)
 #define GPIO_AFRL_AFRL7_AF5 (5U << 28U)
 
-volatile uint32_t data = 0x0000; // SPI data for external DAC
+volatile uint32_t data = 0; // ext DAC data
+volatile uint32_t time = 0; // systick time
 volatile uint8_t data_changed = 0; // flag
 
 #define CS_LOW()  (GPIOA->BSRR = GPIO_BSRR_BR_4) // SPI1
 #define CS_HIGH() (GPIOA->BSRR = GPIO_BSRR_BS_4) // SPI1
 
+// 255 точек синус сдвинутый вверх, максимум 65535
+/*
+// скрипт для создания такой таблицы:
+#include "math.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  // number_of_points_per_period
+  int n = 255;
+  // max DAC data
+  int max_a = 65535;
+  int a = (int)(max_a / 2);
+  for (int i = 0; i < n; i++) {
+    cout << (int)(a * sin(2 * M_PI * i / n) + a) << ", ";
+      if (i % 5 == 4) cout << endl;
+  }
+}
+ */
+uint32_t SINE_LUT[255] = {
+		32500, 33300, 34100, 34900, 35698, 36493, 37287, 38077, 38864, 39648,
+		40427, 41201, 41970, 42733, 43490, 44240, 44983, 45718, 46446, 47165,
+		47875, 48576, 49267, 49948, 50618, 51277, 51925, 52561, 53185, 53796,
+		54395, 54980, 55551, 56109, 56652, 57180, 57694, 58192, 58674, 59141,
+		59592, 60026, 60443, 60843, 61227, 61592, 61940, 62271, 62583, 62877,
+		63152, 63409, 63647, 63866, 64066, 64247, 64409, 64551, 64674, 64777,
+		64861, 64925, 64969, 64994, 64999, 64984, 64950, 64895, 64821, 64728,
+		64615, 64482, 64330, 64159, 63968, 63759, 63530, 63283, 63017, 62732,
+		62429, 62108, 61769, 61412, 61037, 60645, 60236, 59811, 59368, 58910,
+		58435, 57945, 57439, 56918, 56382, 55832, 55267, 54689, 54097, 53492,
+		52875, 52245, 51603, 50949, 50284, 49609, 48923, 48227, 47521, 46807,
+		46083, 45352, 44612, 43866, 43112, 42352, 41586, 40814, 40038, 39257,
+		38471, 37682, 36890, 36096, 35299, 34500, 33700, 32900, 32099, 31299,
+		30499, 29700, 28903, 28109, 27317, 26528, 25742, 24961, 24185, 23413,
+		22647, 21887, 21133, 20387, 19647, 18916, 18192, 17478, 16772, 16076,
+		15390, 14715, 14050, 13396, 12754, 12124, 11507, 10902, 10310, 9732,
+		9167, 8617, 8081, 7560, 7054, 6564, 6089, 5631, 5188, 4763,
+		4354, 3962, 3587, 3230, 2891, 2570, 2267, 1982, 1716, 1469,
+		1240, 1031, 840, 669, 517, 384, 271, 178, 104, 49,
+		15, 0, 5, 30, 74, 138, 222, 325, 448, 590,
+		752, 933, 1133, 1352, 1590, 1847, 2122, 2416, 2728, 3059,
+		3407, 3772, 4156, 4556, 4973, 5407, 5858, 6325, 6807, 7305,
+		7819, 8347, 8890, 9448, 10019, 10604, 11203, 11814, 12438, 13074,
+		13722, 14381, 15051, 15732, 16423, 17124, 17834, 18553, 19281, 20016,
+		20759, 21509, 22266, 23029, 23798, 24572, 25351, 26135, 26922, 27712,
+		28506, 29301, 30099, 30899, 31699
+};
+
 // SysTick
 #define SYSCLK_FREQ   (16000000U)
-#define SysTick_FREQ   (DAC_Saw_freq * MAX_DAC_DATA)
+#define SysTick_FREQ   (1600)
 
 // TODO: SysTick for delays, Timer for DAC
 volatile uint16_t ms_ticks = 0;
@@ -36,11 +85,8 @@ volatile uint16_t ms_ticks = 0;
 // when SysTick counts down zero, increase DAC data
 void SysTick_Handler() {
 	if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) {
-		if (data < 0xFFFF) {
-		    data += 100;
-		} else {
-			data = 0x0;
-		}
+		time = (time + 1) & 0xFF; // чтоб не делать %
+		data = SINE_LUT[time];
 		data_changed = 1;
 	}
 }
@@ -98,15 +144,10 @@ void TIM16_setup() {
 void SPI1_Send(uint16_t data) {
 	// SPI enable -- NSS goes from 1 to 0
 	CS_LOW();
-	//SPI1->CR1 |= SPI_CR1_SPE;
 	while (!(SPI1->SR & SPI_SR_TXE));
 	SPI1->DR = data;
-	while (!(SPI1->SR & SPI_SR_TXE));  // ← добавить: ждём, пока уйдёт в shift register
 	while (SPI1->SR & SPI_SR_BSY);
 	volatile uint16_t dummy = SPI1->DR; // ← сбросить RXNE/OVR
-	// waiting until the end of transmition
-	//while (SPI1->SR & SPI_SR_BSY);
-	//SPI1->CR1 &= ~SPI_CR1_SPE;
 	CS_HIGH();
 }
 
@@ -151,7 +192,7 @@ void SPI1_Setup() {
 
 int main() {
 	// PWM 1000 Hz 50% generation
-	TIM16_setup();
+	//TIM16_setup();
 	// for delay_ms
 	SysTick_setup();
 	SPI1_Setup();
